426f309478636867548006c45ac8fd4f1ec9ac31
cathe <pearsandcabbage@gmail.com>
2026-04-16 13:32:41 +0100

    successful build on nix

--- shell.nix
+++ shell.nix
@@ -1,22 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
-# let
-#   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
-#   pkgs = import nixpkgs {
-#     config = { };
-#     overlays = [ ];
-#   };
-#   inherit (pkgs) stdenv;
-#   # infer = import ./infer.nix {inherit pkgs;};
-#   env =
-#     (pkgs.buildFHSEnv {
-#       name = "fhs env";
-#       targetPkgs = pkgs: [ pkgs.bash ];
-#       runScript = "bash";
-#     }).env;
-# in
-# stdenv.mkDerivation
-# pkgs.mkShell 
-pkgs.buildFHSEnv
+
+(pkgs.buildFHSEnv
 {
   name = "infer-nix";
 
@@ -33,242 +17,26 @@ pkgs.buildFHSEnv
     zlib
     autoconf
     stdenv.cc
-    jdk8
+    # jdk8
     clang
     python3
     erlang
     opam
     go
-    rustc
-    swift
+    # rustc
+    # swift
 
 
-    cowsay
-    lolcat
+    # cowsay
+    # lolcat
   ];
   runScript = "bash";
 
-  # nativeBuildInputs = with pkgs.buildPackages; [
-  #   makeWrapper
-  #   pkg-config
-  #   bash
-  # ];
-
-  # inputsFrom = with pkgs.buildPackages; [
-  #   automake
-  #   cmake
-  #   zlib
-  #   autoconf
-  #   stdenv.cc
-  #   jdk8
-  #   clang
-  #   python3
-  #   erlang
-  #   opam
-  #   go
-  #   rustc
-  #   swift
-  # ];
-
-  # packages = with pkgs; [
-  #   cowsay
-  #   lolcat
-  # ];
-
-  shellHook = ''
-    echo "Building \"./build-infer.sh -y erlang" | lolcat
-    bash ./build-infer.sh -y erlang
-    echo "Running \"make devsetup\"" | lolcat
-    make devsetup
-    echo "Welcome." | cowsay | lolcat
-  '';
-}
-
-# (pkgs.buildFHSEnv {
-#   name = "fhs env";
-#   targetPkgs = pkgs: [ pkgs.bash ];
-#   runScript = "bash";
-# }).env
-
-# pkgs.runInFHSEnv
-# (
-# stdenv.mkDerivation
-# pkgs.mkShell
-# {
-#   name = "infer-nix";
-#   #     version = "1.2.0";
-#   #     system = "x86_64-linux";
-#   nativeBuildInputs = with pkgs.buildPackages; [
-#     makeWrapper
-#     pkg-config
-#     # bash
-#   ];
-
-#   buildInputs = with pkgs.buildPackages; [
-#     automake
-#     cmake
-#     zlib
-#     autoconf
-#     stdenv.cc
-#     jdk8
-#     clang
-#     python3
-#     erlang
-#     opam
-#     go
-#     rustc
-#     swift
-#     (pkgs.buildFHSEnvChroot {
-#       name = "fhs env";
-
-#       targetPkgs = pkgs: [ pkgs.bash ];
-#     }).env
-#   ];
-
-#   packages = with pkgs; [
-#     cowsay
-#     lolcat
-#   ];
-
-#   shellHook = ''
-#     # patchShebangs .
-#     echo "Welcome." | cowsay | lolcat
-#   '';
-# }
-# )
-# {
-#       targetPkgs = pkgs: [ pkgs.bash ];
-
-# }
-
-
-
-
-
-
-
-
-
-
-# { pkgs ? import <nixpkgs> {} }:
-
-# let
-#   # nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
-#   # pkgs = import nixpkgs {
-#   #   config = { };
-#   #   overlays = [ ];
-#   # };
-#   # inherit (pkgs) stdenv;
-#   # infer = import ./infer.nix {inherit pkgs;};
-#   # env =
-#   #   (pkgs.buildFHSEnv {
-#   #     name = "fhs env";
-#   #     targetPkgs = pkgs: [ pkgs.bash ];
-#   #     runScript = "bash";
-#   #   }).env;
-#   theShell = pkgs.mkShell {
-#     nativeBuildInputs = with pkgs.buildPackages; [
-#       makeWrapper
-#       pkg-config
-#       bash
-#     ];
-
-#     buildInputs = with pkgs.buildPackages; [
-#       automake
-#       cmake
-#       zlib
-#       autoconf
-#       stdenv.cc
-#       jdk8
-#       clang
-#       python3
-#       erlang
-#       opam
-#       go
-#       rustc
-#       swift
-#     ];
-
-#     packages = with pkgs; [
-#       cowsay
-#       lolcat
-#     ];
-
-
-#     shellHook = ''
-#       echo "Building \"./build-infer.sh -y erlang\"" | lolcat
-#       bash ./build-infer.sh -y erlang
-#       echo "Running \"make devsetup\"" | lolcat
-#       make devsetup
-#       echo "Welcome." | cowsay | lolcat
-#     '';
-#   };
-
-# in
-# # stdenv.mkDerivation
-# # pkgs.mkShell 
-# pkgs.buildFHSEnv {
-#   name = "infer-nix";
-
-#   targetPkgs = pkgs: theShell.nativeBuildInputs ++ theShell.buildInputs;
-#   runScript = "bash";
-
-#   profile = theShell.shellHook or "";
-# }
-
-# # (pkgs.buildFHSEnv {
-# #   name = "fhs env";
-# #   targetPkgs = pkgs: [ pkgs.bash ];
-# #   runScript = "bash";
-# # }).env
-
-# # pkgs.runInFHSEnv
-# # (
-# # stdenv.mkDerivation
-# # pkgs.mkShell
-# # {
-# #   name = "infer-nix";
-# #   #     version = "1.2.0";
-# #   #     system = "x86_64-linux";
-# #   nativeBuildInputs = with pkgs.buildPackages; [
-# #     makeWrapper
-# #     pkg-config
-# #     # bash
-# #   ];
-
-# #   buildInputs = with pkgs.buildPackages; [
-# #     automake
-# #     cmake
-# #     zlib
-# #     autoconf
-# #     stdenv.cc
-# #     jdk8
-# #     clang
-# #     python3
-# #     erlang
-# #     opam
-# #     go
-# #     rustc
-# #     swift
-# #     (pkgs.buildFHSEnvChroot {
-# #       name = "fhs env";
-
-# #       targetPkgs = pkgs: [ pkgs.bash ];
-# #     }).env
-# #   ];
-
-# #   packages = with pkgs; [
-# #     cowsay
-# #     lolcat
-# #   ];
-
-# #   shellHook = ''
-# #     # patchShebangs .
-# #     echo "Welcome." | cowsay | lolcat
-# #   '';
-# # }
-# # )
-# # {
-# #       targetPkgs = pkgs: [ pkgs.bash ];
-
-# # }
+  # shellHook = ''
+  #   echo "Building \"./build-infer.sh -y erlang" | lolcat
+  #   bash ./build-infer.sh -y erlang
+  #   echo "Running \"make devsetup\"" | lolcat
+  #   make devsetup
+  #   echo "Welcome." | cowsay | lolcat
+  # '';
+}).env
