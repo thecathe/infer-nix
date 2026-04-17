d5048e9bbecccaaf4663e6f904bf07e777a64b77
Jonah Pears <pearsandcabbage@gmail.com>
2026-04-17 11:47:41 +0100

    fixexfor ubuntu

--- shell.nix
+++ shell.nix
@@ -11,13 +11,19 @@ let
       with pkgs;
       with pkgs.buildPackages;
       [
-        # makeWrapper
-        # pkg-config
-        bash
+        ## build tools
+        makeWrapper
+        pkg-config
+        gnumake
         automake
-        cmake
-        zlib
         autoconf
+        cmake
+        bash
+        coreutils
+        which
+        zlib
+        git
+
         opam
         stdenv.cc
         # jdk8
@@ -27,34 +33,68 @@ let
         go
         # rustc
         # swift
+
+        ## opam deps
+        emacs ## required by mem-trace
+        ocamlPackages.findlib
+        sqlite
+        gmp
+        rsync
+
+        lolcat
       ];
 
+    # runScript = ''bash'';
     runScript = ''
       bash -c "
-        eval $(opam env)
+        echo 'Patching PKG_CONFIG_PATH...' | lolcat
+        export PKG_CONFIG_PATH='${pkgs.sqlite.dev}/lib/pkgconfig:${pkgs.sqlite.out}/lib/pkgconfig:$PKG_CONFIG_PATH'
 
-        echo 'Running devsetup...'
+        # echo 'Disabling opam sandboxing and depext...' | lolcat
+        # opam option sandboxing=false
+        # opam option depext=false
+        
+        echo 'Updating opam...' | lolcat
+        opam update
+        
+        echo 'make clean...' | lolcat
+        make clean
+
+        echo 'Building infer...' | lolcat
+        ./build-infer.sh -y erlang
+
+        echo 'Running devsetup...' | lolcat
+        eval \$(opam env)
         make devsetup
 
-        echo 'Checking for local installation .local'
+        echo 'Manually opam installing sqlite3...' | lolcat
+        eval \$(opam env)
+        opam install sqlite3 --yes
+
+        echo 'Checking for local installation .local' | lolcat
         if [ ! -d .local ]; then
-          echo 'Dir .local not found, installing...'
+          echo 'Dir .local not found, installing opam deps...' | lolcat
+          opam install . --deps-only --yes
+          echo 'Installing under $PWD/.local ...' | lolcat
           make install prefix=$PWD/.local
         else
-          echo 'Already installed.'
+          echo 'Already installed.' | lolcat
         fi
 
         ## export path
+        echo 'Patching nix-shell FHS Env path...' | lolcat
         export PATH='$PWD/.local/bin:$PATH'
         
+        echo 'Setup Finished -- Entering shell.' | lolcat
         exec bash
       "
     '';
   };
 in
 pkgs.mkShell {
-  packages = [ fhs ];
+  packages = [ fhs pkgs.lolcat ];
   shellHook = ''
+    echo "Entering nix-shell FSH Env to complete setup..." | lolcat
     exec infer-nix
   '';
 }
